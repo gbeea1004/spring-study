aed36ed35a1edeb15f06db41a9aa35adbc0099d6
gbeea1004 <jvckigh@gmail.com>
2021-06-15 00:17:49 +0900

    리소스를 읽어오는 기능을 제공하는 인터페이스 : ResourceLoader

--- src/main/java/com/geon/springstudy/AppRunner.java
+++ src/main/java/com/geon/springstudy/AppRunner.java
@@ -3,16 +3,23 @@ package com.geon.springstudy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    private ApplicationEventPublisher publisherEvent;
+    private ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        publisherEvent.publishEvent(new MyEvent(this, 100));
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.exists());
+        System.out.println(resource.getDescription());
+        System.out.println(Files.readString(Path.of(resource.getURI())));
     }
 }
--- src/main/java/com/geon/springstudy/SpringStudyApplication.java
+++ src/main/java/com/geon/springstudy/SpringStudyApplication.java
@@ -2,10 +2,8 @@ package com.geon.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
