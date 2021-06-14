287643399530ce55b2d31c6bd5fb33ca15b3917c
gbeea1004 <jvckigh@gmail.com>
2021-06-14 20:03:53 +0900

    국제화 (i18n) : messages.properties 적용

--- src/main/java/com/geon/springstudy/AppRunner.java
+++ src/main/java/com/geon/springstudy/AppRunner.java
@@ -1,29 +1,23 @@
 package com.geon.springstudy;
 
-import com.geon.springstudy.domain.book.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.core.env.Environment;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
     @Autowired
-    private ApplicationContext ac;
-
-    @Autowired
-    private BookRepository bookRepository;
-
-    @Value("${app.name}")
-    String appName;
+    private MessageSource messageSource;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Environment environment = ac.getEnvironment();
-        System.out.println(environment.getProperty("app.name"));
-        System.out.println(appName);
+        System.out.println(messageSource.getClass());
+        System.out.println(messageSource.getMessage("greeting", new String[]{"geonHee"}, Locale.KOREA));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"geonHee"}, Locale.US));
+        System.out.println(messageSource.getMessage("greeting", new String[]{"geonHee"}, Locale.getDefault())); // 한국어가 기본값이므로 한국어가 적용됨
     }
 }
