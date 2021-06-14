805e7307b0e206199a576b970b3c2b723d063cd0
gbeea1004 <jvckigh@gmail.com>
2021-06-14 17:21:28 +0900

    싱글톤 빈이 프로토타입 빈을 참조하는 경우, Object-Provider 사용
    - 단 이 방법은 스프링에 의존하게 되므로 추천하지 않음.
    - 대부분의 경우 싱글톤 빈으로 사용하므로 참고로 알아두기.

--- src/main/java/com/geon/springstudy/domain/scope/Single.java
+++ src/main/java/com/geon/springstudy/domain/scope/Single.java
@@ -1,14 +1,15 @@
 package com.geon.springstudy.domain.scope;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Single {
     @Autowired
-    private Proto proto;
+    private ObjectProvider<Proto> proto;
 
     public Proto getProto() {
-        return proto;
+        return proto.getIfAvailable();
     }
 }
