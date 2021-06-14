3bb5850d7c64231643f246238083f2761db51da4
gbeea1004 <jvckigh@gmail.com>
2021-06-14 15:40:28 +0900

    자바 설정파일 component-scan 을 통한 빈 주입
    - 사실 이 방법은 쓸 필요가 없다.
      main 클래스의 @SpringBootApplication 을 통해 스캔을 하기 때문

--- src/main/java/com/geon/springstudy/domain/book/BookRepository.java
+++ src/main/java/com/geon/springstudy/domain/book/BookRepository.java
@@ -1,6 +1,8 @@
 package com.geon.springstudy.domain.book;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookRepository {
     public Book save(Book book) {
         return null;
