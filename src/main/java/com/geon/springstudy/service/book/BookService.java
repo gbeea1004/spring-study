cea0031fc3d20185d4d64ec2bdddd50729835cbf
gbeea1004 <jvckigh@gmail.com>
2021-06-14 16:12:10 +0900

    빈 라이프사이클 @PostConstruct 사용하기

--- src/main/java/com/geon/springstudy/service/book/BookService.java
+++ src/main/java/com/geon/springstudy/service/book/BookService.java
@@ -4,15 +4,21 @@ import com.geon.springstudy.domain.book.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
-    private List<BookRepository> bookRepositories;
+    private BookRepository geonHeeBookRepository;
 
 
     public void printBookRepository() {
-        bookRepositories.forEach(System.out::println);
+        System.out.println(geonHeeBookRepository.getClass());
+    }
+
+    @PostConstruct
+    public void testPostConstruct() {
+        System.out.println("testPostConstruct -> " + geonHeeBookRepository.getClass());
     }
 }
