857a974647019458d4d10ffaf2c0d479adb44b60
gbeea1004 <jvckigh@gmail.com>
2021-06-14 17:44:21 +0900

    프로파일 : 메서드에 정의

--- src/main/java/com/geon/springstudy/config/AppConfig.java
+++ src/main/java/com/geon/springstudy/config/AppConfig.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("test")
 public class AppConfig {
 
     @Bean
+    @Profile("test")
     public BookRepository bookRepository() {
         return new TestBookRepository();
     }
