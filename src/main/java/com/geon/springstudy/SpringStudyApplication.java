60882cd3a29fec24829cc68900d8d79d32a165c2
gbeea1004 <jvckigh@gmail.com>
2021-06-15 00:03:28 +0900

    이벤트 프로그래밍 : 스프링이 제공하는 기본 이벤트
    - ContextRefreshedEvent
    - ContextStartedEvent
    - ContextStoppedEvent
    - ContextClosedEvent

--- src/main/java/com/geon/springstudy/SpringStudyApplication.java
+++ src/main/java/com/geon/springstudy/SpringStudyApplication.java
@@ -2,8 +2,10 @@ package com.geon.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringStudyApplication {
 
     public static void main(String[] args) {
